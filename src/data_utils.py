6a576e4eef1427bc135ab1fb7aef3cf39e576b4e
Christian RG <christian.rojasg@pucp.edu.pe>
2025-04-08 20:12:35 -0500

    Update data_utils.py

--- src/data_utils.py
+++ src/data_utils.py
@@ -69,6 +69,12 @@ def get_feature_target(
     """
     X_train, y_train, X_test, y_test = None, None, None, None
 
+
+    X_train = app_train.drop(columns=["TARGET"], axis=1)
+    y_train = app_train["TARGET"]
+    X_test = app_test.drop(columns=["TARGET"], axis=1)
+    y_test = app_test["TARGET"]
+
     # TODO
     # Assign to X_train all the columns from app_train except "TARGET"
     # Assign to y_train the "TARGET" column
@@ -101,16 +107,13 @@ def get_train_val_sets(
         y_val : pd.Series
             Validation target
     """
-    X_train, X_val, y_train, y_val = None, None, None, None
-
-    # TODO
-    # Use the `sklearn.model_selection.train_test_split` function with
-    # `X_train`, `y_train` datasets.
-    # Assign only 20% of the dataset for testing (see `test_size` parameter in
-    # `train_test_split`).
-    # Assign a seed so we get reproducible output across multiple function
-    # calls (see `random_state` parameter in `train_test_split`).
-    # Shuffle the data (see `shuffle` parameter in `train_test_split`).
-
+    # Dividir los datos en conjuntos de entrenamiento y validación
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train, 
+        y_train, 
+        test_size=0.2,      
+        random_state=42,       
+        shuffle=True           
+    )
 
     return X_train, X_val, y_train, y_val
